3cff329065cbcd735157a951bd11cd5fe6b28b9e
mohamad Salman <mohamad.salman@etu.univ-amu.fr>
2025-11-25 13:36:05 +0100

    création de l'interface boardcontext

---
+++ src/main/java/model/BoardContext.java
@@ -0,0 +1,18 @@
+package model;
+import util.Position;
+import util.TargetStrategy;
+import java.util.*;
+
+public interface BoardContext {
+    public int stepNumber();
+    List<Position> getNeighbors(Position p);
+    public Map<Position, List<Position>> getNeighborsMap() ;
+    Set<Position> getFirePositions();
+    public void extinguish(Position position);
+    public void  createFire(Position position);
+    public int rowCount();
+    public int columnCount();
+    public Position randomPosition();
+    boolean isOccupied(Position position);
+
+}
